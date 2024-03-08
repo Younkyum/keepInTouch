9e9d0338d924c4bcf6001b1e4413db9b75b88b7f
Younkyum <jinyounkyum@hufs.ac.kr>
2024-03-08 14:46:54 +0900

    Initial Commit

---
+++ keepInTouch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keepInTouch
+//
+//  Created by YounkyumJin on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keepInTouch/keepInTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepInTouchApp.swift
+//  keepInTouch
+//
+//  Created by YounkyumJin on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct keepInTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
